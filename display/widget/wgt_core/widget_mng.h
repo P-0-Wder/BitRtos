616f4aac6410e008ba48fd27aac047483a0224cb
8BIT0 <971168718@qq.com>
2021-08-08 23:00:17 +0800

    widget update

--- display/widget/wgt_core/widget_mng.h
+++ display/widget/wgt_core/widget_mng.h
@@ -15,9 +15,11 @@ typedef uint32_t Widget_Handle;
 #define WIDGET_MAX_WIDTH 256
 #define WIDGET_MAX_HEIGHT 64
 #define MAX_WIDGET_CACHE_PAGE 10
+#define DEFAULT_LAYER 0
 
 typedef enum
 {
+    Widget_Created,
     Widget_Show,
     Widget_Hide,
 } Widget_State_TypeDef;
@@ -28,16 +30,13 @@ typedef struct
     uint8_t on_layer;
     uint8_t cord_x;
     uint8_t cord_y;
-
     uint8_t width;
     uint8_t height;
-
     uint8_t **cache;
-
     char *name;
 } Widget_TypeDef;
 
-Widget_Handle Widget_Create(uint8_t cord_x, uint8_t cord_y, uint8_t width, uint8_t height);
+Widget_Handle Widget_Create(uint8_t cord_x, uint8_t cord_y, uint8_t width, uint8_t height, char *name);
 bool Widget_Deleted(Widget_Handle *hdl);
 
 #endif
