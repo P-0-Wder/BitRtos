0b6fc0539a75c8f78b5fe3f8bbf2d650ec7cbf53
8BIT0 <971168718@qq.com>
2021-08-17 18:49:57 +0800

    task function update

---
+++ App/Task_Display/Task_Widget.c
@@ -0,0 +1,6 @@
+#include "Task_Widget.h"
+#include "task_manager.h"
+
+void TaskWidget_Core(Task_Handler self)
+{
+}
---
+++ App/Task_Display/Task_Widget.h
@@ -0,0 +1,16 @@
+#ifndef __TASK_WIDGET_H
+#define __TASK_WIDGET_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef enum
+{
+    TaskWdiget_State_Init,
+    TaskWidget_State_CreatedWidget,
+    TaskWidget_State_Run,
+} TaskWidget_State_List;
+
+void TaskWidget_Core(Task_Handler self);
+
+#endif
