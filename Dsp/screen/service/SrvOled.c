8912ea41520c17bc69ba0219c952f77b6ec67fb2
8BIT0 <971168718@qq.com>
2021-08-17 22:52:18 +0800

    srv update

--- Dsp/screen/service/SrvOled.c
+++ Dsp/screen/service/SrvOled.c
@@ -6,17 +6,30 @@ Oled_Obj_TypeDef Oled1306Obj;
 
 /* internal function definition */
 static bool SrvOled_PreInit(void);
+static void SrvOled_BusInit(SPI_List BusID);
 
 /* external function definition */
 static bool SrvOled_Init(void);
 static void SrvOled_Fresh(uint8_t **bit_map);
+
+static void SrvOled_BusInit(SPI_List BusID)
+{
+    DrvSpi_Obj_TypeDef BusInit_Structure;
+
+    BusInit_Structure.SPIx = BusID;
+    BusInit_Structure.speed = SPI_CLOCK_INITIALIZATON;
+    BusInit_Structure.CPOL = ;
+    BusInit_Structure.CPHA = ;
+
+    GenSPI_Drv.open(&BusInit_Structure);
+}
 
 static bool SrvOled_PreInit(void)
 {
     Oled1306Obj.Bus = Oled_SpiBus;
     Oled1306Obj.BusID = SPI_1;
 
-    Oled1306Obj.bus_init = GenSPI_Drv.open();
+    Oled1306Obj.bus_init = SrvOled_BusInit;
 }
 
 static bool SrvOled_Init(void)
