4d910dedb7fbce9bbbd13251a44c35efc2f227c0
8BIT0 <971168718@qq.com>
2021-08-29 22:48:30 +0800

    update

--- App/Task_Display/Task_Widget.c
+++ App/Task_Display/Task_Widget.c
@@ -7,6 +7,7 @@ static TaskWidget_State_List TaskWidget_State = TaskWdiget_State_Init;
 static Widget_Handle test1;
 static Widget_Handle test2;
 static Widget_Handle test3;
+static Widget_Handle test4;
 
 /* external function definition */
 Task_Handler TaskWidget_Hdl = 0;
@@ -19,18 +20,22 @@ static void TaskWidget_Init(void)
 
     Widget_Mng.config_all->mirror(Oled_MirrorX);
 
-    test1 = Widget_Mng.Create(0, 0, 30, 20, "test1", true);
-    test2 = Widget_Mng.Create(35, 5, 40, 20, "test2", true);
-    test3 = Widget_Mng.Create(20, 15, 80, 20, "test3", true);
+    test1 = Widget_Mng.Create(20, 2, 25, 20, "test1", true);
+    test2 = Widget_Mng.Create(50, 5, 50, 20, "test2", true);
+    test3 = Widget_Mng.Create(15, 15, 100, 40, "test3", true);
+    test4 = Widget_Mng.Create(0, 0, 124, 64, "test4", true);
+
+    Widget_Mng.Control(test4)->Draw()->draw_char(Font_12, '4', 8, 2, true);
+    Widget_Mng.Control(test4)->Show();
 
     Widget_Mng.Control(test1)->Draw()->draw_char(Font_12, '1', 8, 2, true);
     Widget_Mng.Control(test1)->Show();
 
-    Widget_Mng.Control(test2)->Draw()->draw_char(Font_12, '2', 8, 2, true);
-    Widget_Mng.Control(test2)->Show();
-
     Widget_Mng.Control(test3)->Draw()->draw_char(Font_12, '3', 8, 2, true);
     Widget_Mng.Control(test3)->Show();
+
+    Widget_Mng.Control(test2)->Draw()->draw_char(Font_12, '2', 8, 2, true);
+    Widget_Mng.Control(test2)->Show();
 }
 
 void TaskWidget_Core(Task_Handler self)
