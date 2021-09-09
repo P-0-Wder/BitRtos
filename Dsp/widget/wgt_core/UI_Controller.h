4fc444ebb81e086865bc7562736f1439db58ec73
8BIT0 <971168718@qq.com>
2021-09-09 11:37:15 +0800

    ui controller update

--- Dsp/widget/wgt_core/UI_Controller.h
+++ Dsp/widget/wgt_core/UI_Controller.h
@@ -5,14 +5,44 @@
 #include <stdint.h>
 #include <string.h>
 
+typedef enum
+{
+    ProcBar_Move_Left = 0,
+    ProcBar_Move_Right,
+} ProcessBar_MoveDir_TypeDef;
+
 typedef struct
 {
-    /* data */
+    char *label;
+    uint8_t x;
+    uint8_t y;
+
+    uint8_t line_width;
+    bool label_roll;
+    bool label_dsp;
+} UI_GeneralData_TypeDef;
+
+typedef struct
+{
+    UI_GeneralData_TypeDef Gen_Data;
+
+    float percent;
+    uint32_t range;
+    uint32_t cur_var;
+
+    uint8_t width;
+    uint8_t height;
 } UI_ProcessBarObj_TypeDef;
 
 typedef struct
 {
-    /* data */
+    UI_GeneralData_TypeDef Gen_Data;
+
+    float percent;
+    uint32_t range;
+    uint32_t cur_val;
+
+    uint8_t radius;
 } UI_ProcessCircleObj_TypeDef;
 
 typedef struct
@@ -32,34 +62,54 @@ typedef struct
 
 typedef struct
 {
+    char *label;
+    uint8_t item_num;
     /* data */
 } UI_ComboBox_Group_TypeDef;
 
 typedef struct
 {
+    UI_GeneralData_TypeDef Gen_Data;
+    uint8_t item_num;
     uint32_t group_prt;
     /* data */
 } UI_ComboBoxObj_TypeDef;
 
 typedef struct
 {
+    UI_GeneralData_TypeDef Gen_Data;
+    uint8_t frame_size;
     /* data */
 } UI_ButtonObj_TypeDef;
 
 typedef struct
 {
-    /* data */
+    UI_GeneralData_TypeDef Gen_Data;
+    uint8_t max_item_num;
+    uint8_t item_height;
+    uint8_t item_width;
 } UI_DropListObj_TypeDef;
 
 typedef struct
 {
-    /* data */
 } UI_DropItemObj_TypeDef;
+
+typedef struct
+{
+    UI_GeneralData_TypeDef Gen_Data;
+    int32_t range;
+} UI_DigInputObj_TypeDef;
+
+typedef struct
+{
+    UI_GeneralData_TypeDef Gen_Data;
+    uint8_t max_input_len;
+} UI_StrInputObj_TypeDef;
 
 /* ui process function block */
 typedef struct
 {
-    /* data */
+
 } UI_Utils_TypeDef;
 
 #endif
