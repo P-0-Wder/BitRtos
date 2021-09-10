bdfd6bb62e0dfa29ba45af6a9f50a83b6d259b40
8BIT0 <971168718@qq.com>
2021-09-10 23:13:47 +0800

    update

--- Dsp/widget/wgt_core/UI_Controller.c
+++ Dsp/widget/wgt_core/UI_Controller.c
@@ -6,28 +6,52 @@
 
 /******************************* init function *********************************/
 
-static void UI_ProcessBar_Init(uint8_t **map, UI_ProcessBarObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t pcnt)
+static bool UI_ProcessBar_Init(uint8_t **map, UI_ProcessBarObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t pcnt)
 {
+    if ((map == NULL) || (Obj == NULL))
+        return false;
+
+    return true;
 }
 
-static void UI_VerticlBar_Init(uint8_t **map, UI_VerticalBarObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t unit_len)
+static bool UI_VerticlBar_Init(uint8_t **map, UI_VerticalBarObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t unit_len)
 {
+    if ((map == NULL) || (Obj == NULL))
+        return false;
+
+    return true;
 }
 
-static void UI_HorizonBar_Init(uint8_t **map, UI_HorizonBarObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t unit_len)
+static bool UI_HorizonBar_Init(uint8_t **map, UI_HorizonBarObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t unit_len)
 {
+    if ((map == NULL) || (Obj == NULL))
+        return false;
+
+    return true;
 }
 
-static void UI_ProcessCircle_Init(uint8_t **map, UI_ProcessCircleObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t radius, uint8_t line_width, uint8_t pcnt)
+static bool UI_ProcessCircle_Init(uint8_t **map, UI_ProcessCircleObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t radius, uint8_t line_width, uint8_t pcnt)
 {
+    if ((map == NULL) || (Obj == NULL))
+        return false;
+
+    return true;
 }
 
-static void UI_CheckBox_Init(uint8_t **map, UI_CheckBoxObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, bool state)
+static bool UI_CheckBox_Init(uint8_t **map, UI_CheckBoxObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, bool state)
 {
+    if ((map == NULL) || (Obj == NULL))
+        return false;
+
+    return true;
 }
 
-static void UI_ComboBox_Init(uint8_t **map, UI_ComboBoxObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t radius, uint8_t state)
+static bool UI_ComboBox_Init(uint8_t **map, UI_ComboBoxObj_TypeDef *Obj, char *label, uint8_t x, uint8_t y, uint8_t radius, uint8_t state)
 {
+    if ((map == NULL) || (Obj == NULL))
+        return false;
+
+    return true;
 }
 
 /******************************* control function *********************************/
