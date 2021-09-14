411cd6d1d3172c4240b342c0cbc71ca3ab4002f0
8BIT0 <971168718@qq.com>
2021-09-15 00:01:04 +0800

    drv serial add

--- Drv/drv_serial.c
+++ Drv/drv_serial.c
@@ -1,2 +1,20 @@
 #include "drv_serial.h"
 #include "periph_serial.h"
+
+/* internal virable */
+
+/* internal function */
+
+/* external function */
+
+static bool DrvSerial_Ctl()
+{
+}
+
+static bool DrvSerial_Read()
+{
+}
+
+static bool DrvSerial_Write()
+{
+}
--- Drv/drv_serial.h
+++ Drv/drv_serial.h
@@ -1,6 +1,8 @@
 #ifndef DRV_SERIAL_H
 #define DRV_SERIAL_H
 
+#include <stdbool.h>
+
 typedef struct
 {
     /* data */
