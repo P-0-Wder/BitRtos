8372db2e48b4a431f2e7ac0c7a18687ad677f30f
8BIT0 <971168718@qq.com>
2021-09-20 23:01:39 +0800

    bsp adc add

--- Bsp/periph_adc.c
+++ Bsp/periph_adc.c
@@ -1,32 +1,32 @@
 #include "periph_adc.h"
+#include "periph_gpio.h"
 #include "stm32f4xx_adc.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
 
 static void (*ADC_IO_Init[])(void) = {
-    GPIO_USART1_IO_Init,
-    GPIO_USART2_IO_Init,
-    GPIO_USART3_IO_Init,
-    GPIO_USART6_IO_Init,
+    GPIO_ADC1_Channel0_IO_Init,
+    GPIO_ADC1_Channel1_IO_Init,
+    GPIO_ADC1_Channel2_IO_Init,
+    GPIO_ADC1_Channel3_IO_Init,
+    GPIO_ADC1_Channel4_IO_Init,
+    GPIO_ADC1_Channel5_IO_Init,
+    GPIO_ADC1_Channel6_IO_Init,
+    GPIO_ADC1_Channel7_IO_Init,
+    GPIO_ADC1_Channel8_IO_Init,
+    GPIO_ADC1_Channel9_IO_Init,
+    GPIO_ADC1_Channel10_IO_Init,
+    GPIO_ADC1_Channel11_IO_Init,
+    GPIO_ADC1_Channel12_IO_Init,
+    GPIO_ADC1_Channel13_IO_Init,
+    GPIO_ADC1_Channel14_IO_Init,
 };
 
-static void Periph_ADC_IO_Init()
+void Periph_ADC_Init(ADC_Channel_List ADCx_Channelx)
 {
-    GPIO_InitTypeDef GPIO_InitStruct;
-
-    RCC_AHB1PeriphClockCmd(, ENABLE);
-
-    GPIO_InitStruct.GPIO_Mode = ;
-    GPIO_InitStruct.GPIO_OType = ;
-    GPIO_InitStruct.GPIO_Pin = ;
-    GPIO_InitStruct.GPIO_PuPd = ;
-    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    ADC_IO_Init[ADCx_Channelx]();
 }
 
-static void Periph_ADC_Init()
-{
-}
-
-static void Periph_ADC_GetValue()
+void Periph_ADC_GetValue()
 {
 }
--- Bsp/periph_adc.h
+++ Bsp/periph_adc.h
@@ -3,7 +3,21 @@
 
 typedef enum
 {
-
-} periph_adc_list;
+    ADC1_Channel0 = 0,
+    ADC1_Channel1,
+    ADC1_Channel2,
+    ADC1_Channel3,
+    ADC1_Channel4,
+    ADC1_Channel5,
+    ADC1_Channel6,
+    ADC1_Channel7,
+    ADC1_Channel8,
+    ADC1_Channel9,
+    ADC1_Channel10,
+    ADC1_Channel11,
+    ADC1_Channel12,
+    ADC1_Channel13,
+    ADC1_Channel14,
+} ADC_Channel_List;
 
 #endif
