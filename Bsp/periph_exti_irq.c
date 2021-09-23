fd1754c226af832d1bd63008081df7df0849da82
8BIT0 <971168718@qq.com>
2021-09-23 15:29:36 +0800

    periph_exti_irq add

--- Bsp/periph_exti_irq.c
+++ Bsp/periph_exti_irq.c
@@ -1,3 +1,62 @@
 #include "periph_exti_irq.h"
 #include "stm32f4xx_exti.h"
 #include "periph_gpio.h"
+
+void EXTI0_IRQnHandler(void)
+{
+}
+
+void EXTI1_IRQnHandler(void)
+{
+}
+
+void EXTI2_IRQnHandler(void)
+{
+}
+
+void EXTI3_IRQnHandler(void)
+{
+}
+void EXTI4_IRQnHandler(void)
+{
+}
+
+void EXTI5_IRQnHandler(void)
+{
+}
+
+void EXTI6_IRQnHandler(void)
+{
+}
+
+void EXTI7_IRQnHandler(void)
+{
+}
+
+void EXTI8_IRQnHandler(void)
+{
+}
+
+void EXTI9_IRQnHandler(void)
+{
+}
+
+void EXTI10_IRQnHandler(void)
+{
+}
+
+void EXTI11_IRQnHandler(void)
+{
+}
+
+void EXTI12_IRQnHandler(void)
+{
+}
+
+void EXTI13_IRQnHandler(void)
+{
+}
+
+void EXTI14_IRQnHandler(void)
+{
+}
