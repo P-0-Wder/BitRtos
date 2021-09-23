a070c3cff5d3106defece87d8363990960fb598d
8BIT0 <971168718@qq.com>
2021-09-23 15:25:04 +0800

    periph exti update

--- Bsp/periph_exti.c
+++ Bsp/periph_exti.c
@@ -2,10 +2,21 @@
 #include "periph_nvic.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_exti.h"
+#include "stm32f4xx_syscfg.h"
 
 void periph_exti_init()
 {
     EXTI_InitTypeDef EXTI_InitStruct;
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    SYSCFG_EXTILineConfig();
+
+    EXTI_InitStruct.EXTI_Line = ;
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+
+    EXTI_Init(&EXTI_InitStruct);
+
+    periph_nvic_Structure_Setting();
 }
