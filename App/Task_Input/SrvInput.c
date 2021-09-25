27833b0701c4c3a8f09f257cdc36b5ae9b9e4cea
8BIT0 <971168718@qq.com>
2021-09-25 23:18:22 +0800

    SrvInput update

--- App/Task_Input/SrvInput.c
+++ App/Task_Input/SrvInput.c
@@ -35,15 +35,15 @@ static void SrvInput_Init(void)
     DrvADC.ctl(ADC_Config_IO, ADC1_Channel1);
 
     /* right gimbal io init */
-    DrvADC.ctl(ADC_Config_IO, ADC1_Channel2);
-    DrvADC.ctl(ADC_Config_IO, ADC1_Channel3);
+    DrvADC.ctl(ADC_Config_IO, ADC1_Channel4);
+    DrvADC.ctl(ADC_Config_IO, ADC1_Channel6);
 
     /* pot on both side io init */
-    DrvADC.ctl(ADC_Config_IO, ADC1_Channel4);
-    DrvADC.ctl(ADC_Config_IO, ADC1_Channel5);
+    DrvADC.ctl(ADC_Config_IO, ADC1_Channel8);
+    DrvADC.ctl(ADC_Config_IO, ADC1_Channel9);
 
     /* voltage detect */
-    DrvADC.ctl(ADC_Config_IO, ADC1_Channel6);
+    DrvADC.ctl(ADC_Config_IO, ADC1_Channel10);
 
     /* toggle io init */
     DrvGPIO.open(&Toggle_Left, GPIO_Input, NULL);
