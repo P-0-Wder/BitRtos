006e18d89ba1a98c22ed99da509dfe626b4fdf0d
8BIT0 <971168718@qq.com>
2021-10-05 23:21:25 +0800

    dev_gimbal update

--- Dev/Dev_Gimbal.c
+++ Dev/Dev_Gimbal.c
@@ -1,4 +1,6 @@
 #include "Dev_Gimbal.h"
+#include "drv_adc.h"
+#include "drv_gpio.h"
 
 /* internal variable */
 static uint8_t gimbal_cnt = 0;
@@ -6,6 +8,8 @@ static uint8_t gimbal_cnt = 0;
 /* internal function */
 
 /* external variable */
+DevGimbal_TypeDef DevGimbal_Left = {};
+DevGimbal_TypeDef DevGimbal_Right = {};
 
 static bool DevGimbal_Open()
 {
--- Dev/Dev_Gimbal.h
+++ Dev/Dev_Gimbal.h
@@ -4,4 +4,32 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef enum
+{
+    Gimbal_AxisX_Invert = 1 << 0,
+    Gimbal_AxisY_Invert = 1 << 1,
+} DevGimbal_Invert_List;
+
+typedef struct
+{
+    uint8_t invert_reg;
+
+    int16_t Offset_X;
+    int16_t Offset_Y;
+} DevGimbal_Obj_TypeDef;
+
+typedef struct
+{
+    int16_t Gim_X;
+    int16_t Gim_Y;
+} DevGimbal_Val_TypeDef;
+
+typedef struct
+{
+    bool (*open)(DevGimbal_Obj_TypeDef *gimbal);
+    bool (*invert)(DevGimbal_Obj_TypeDef *gimbal);
+    bool (*set_offset)(DevGimbal_Obj_TypeDef *gimbal, int16_t offset_x, int16_t offset_y);
+    DevGimbal_Val_TypeDef (*get)(DevGimbal_Obj_TypeDef *gimbal);
+} DevGimbal_TypeDef;
+
 #endif
