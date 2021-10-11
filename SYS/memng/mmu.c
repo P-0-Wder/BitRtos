a979b9587d306ad111d852e9988cc036e516e307
8BIT0 <971168718@qq.com>
2021-10-11 11:04:06 +0800

    mmu update

--- SYS/memng/mmu.c
+++ SYS/memng/mmu.c
@@ -1,2 +1,29 @@
 #include "mmu.h"
 #include <string.h>
+#include <stdio.h>
+
+/* internal variable */
+static Mem_Monitor_TypeDef Mem_Monitor = {
+    .remain_size = PHY_MEM_SIZE,
+    .total_size = PHY_MEM_SIZE,
+    .used_size = 0,
+};
+
+static uint8_t MMU_Buff[PHY_MEM_SIZE] __attribute__((__align(32)));
+
+static MMU_Trim(void)
+{
+}
+
+void *MMU_Malloc(uint32_t size)
+{
+}
+
+void MMU_Free(void *ptr)
+{
+}
+
+Mem_Monitor_TypeDef MMU_Info(void)
+{
+    return Mem_Monitor;
+}
--- SYS/memng/mmu.h
+++ SYS/memng/mmu.h
@@ -9,4 +9,11 @@
 
 #define PHY_MEM_SIZE 64 * 1024
 
+typedef struct
+{
+    uint32_t used_size;
+    uint32_t remain_size;
+    uint32_t total_size;
+} Mem_Monitor_TypeDef;
+
 #endif
