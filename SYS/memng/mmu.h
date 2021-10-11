e812c4265d7eb95bcdf5750f1fbdd173e4d18bd7
8BIT0 <971168718@qq.com>
2021-10-11 10:22:38 +0800

    mmu update

---
+++ SYS/memng/mmu.h
@@ -0,0 +1,12 @@
+#ifndef __MMU_H
+#define __MMU_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PHY_MEM_SIZE 64 * 1024
+
+#endif
