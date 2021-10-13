14202f6a72f1d1098be78aaeac84474ba6209d28
8BIT0 <971168718@qq.com>
2021-10-13 21:15:02 +0800

    bsp serial dma send update

--- Bsp/periph_serial.c
+++ Bsp/periph_serial.c
@@ -232,7 +232,8 @@ void Serial_SendBuff(USART_TypeDef *Serial_port, char *Buff, uint16_t Len)
 {
 	for (uint8_t Buff_index = 0; Buff_index < Len; Buff_index++)
 	{
-		while (USART_GetFlagStatus(Serial_port, USART_FLAG_TC) == RESET);
+		while (USART_GetFlagStatus(Serial_port, USART_FLAG_TC) == RESET)
+			;
 		USART_SendData(Serial_port, Buff[Buff_index]);
 	}
 }
@@ -251,63 +252,66 @@ void Serial_DMA_TX_IRQSetting(Serial_List serial_id)
 void Serial_DMA_SendBuff(Serial_List serial_id, uint16_t len)
 {
 	DMA_Cmd(Serial_DMA_TX_Stream[serial_id], DISABLE);
-	while (DMA_GetCmdStatus(Serial_DMA_TX_Stream[serial_id]) != DISABLE){}	//确保DMA可以被设置  
+	while (DMA_GetCmdStatus(Serial_DMA_TX_Stream[serial_id]) != DISABLE)
+	{
+	} //确保DMA可以被设置
 	DMA_SetCurrDataCounter(Serial_DMA_TX_Stream[serial_id], (uint16_t)len);
 	DMA_Cmd(Serial_DMA_TX_Stream[serial_id], ENABLE);
+
+	while (DMA_GetFlagStatus(Serial_DMA_TX_Stream[serial_id], DMA_TX_FinishFlag[serial_id]) != RESET)
+		;
 }
 
-
-void MYDMA_Config(DMA_Stream_TypeDef *DMA_Streamx,u32 chx,u32 par,u32 mar,u16 ndtr)
-{ 
- 
-	DMA_InitTypeDef  DMA_InitStructure;
-	
-	if((u32)DMA_Streamx>(u32)DMA2)//得到当前stream是属于DMA2还是DMA1
-	{
-	  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2,ENABLE);//DMA2时钟使能 
-		
-	}else 
-	{
-	  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1,ENABLE);//DMA1时钟使能 
-	}
-  DMA_DeInit(DMA_Streamx);
-	
-	while (DMA_GetCmdStatus(DMA_Streamx) != DISABLE){}//等待DMA可配置 
-	
-  /* 配置 DMA Stream */
-  DMA_InitStructure.DMA_Channel = chx;  //通道选择
-  DMA_InitStructure.DMA_PeripheralBaseAddr = par;//DMA外设地址
-  DMA_InitStructure.DMA_Memory0BaseAddr = mar;//DMA 存储器0地址
-  DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;//存储器到外设模式
-  DMA_InitStructure.DMA_BufferSize = ndtr;//数据传输量 
-  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;//外设非增量模式
-  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;//存储器增量模式
-  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;//外设数据长度:8位
-  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;//存储器数据长度:8位
-  DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;// 使用普通模式 
-  DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;//中等优先级
-  DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;         
-  DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
-  DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;//存储器突发单次传输
-  DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;//外设突发单次传输
-  DMA_Init(DMA_Streamx, &DMA_InitStructure);//初始化DMA Stream
-	
-
-} 
-//开启一次DMA传输
-//DMA_Streamx:DMA数据流,DMA1_Stream0~7/DMA2_Stream0~7 
-//ndtr:数据传输量  
-void MYDMA_Enable(DMA_Stream_TypeDef *DMA_Streamx,u16 ndtr)
+void MYDMA_Config(DMA_Stream_TypeDef *DMA_Streamx, u32 chx, u32 par, u32 mar, u16 ndtr)
 {
- 
-	DMA_Cmd(DMA_Streamx, DISABLE);                      //关闭DMA传输 
-	
-	while (DMA_GetCmdStatus(DMA_Streamx) != DISABLE){}	//确保DMA可以被设置  
-		
-	DMA_SetCurrDataCounter(DMA_Streamx,ndtr);          //数据传输量  
- 
-	DMA_Cmd(DMA_Streamx, ENABLE);                      //开启DMA传输 
-}	  
 
+	DMA_InitTypeDef DMA_InitStructure;
 
+	if ((u32)DMA_Streamx > (u32)DMA2) //得到当前stream是属于DMA2还是DMA1
+	{
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE); //DMA2时钟使能
+	}
+	else
+	{
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE); //DMA1时钟使能
+	}
+	DMA_DeInit(DMA_Streamx);
 
+	while (DMA_GetCmdStatus(DMA_Streamx) != DISABLE)
+	{
+	} //等待DMA可配置
+
+	/* 配置 DMA Stream */
+	DMA_InitStructure.DMA_Channel = chx;									//通道选择
+	DMA_InitStructure.DMA_PeripheralBaseAddr = par;							//DMA外设地址
+	DMA_InitStructure.DMA_Memory0BaseAddr = mar;							//DMA 存储器0地址
+	DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;					//存储器到外设模式
+	DMA_InitStructure.DMA_BufferSize = ndtr;								//数据传输量
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;		//外设非增量模式
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;					//存储器增量模式
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte; //外设数据长度:8位
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;			//存储器数据长度:8位
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;							// 使用普通模式
+	DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;					//中等优先级
+	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+	DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
+	DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;			//存储器突发单次传输
+	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single; //外设突发单次传输
+	DMA_Init(DMA_Streamx, &DMA_InitStructure);							//初始化DMA Stream
+}
+//开启一次DMA传输
+//DMA_Streamx:DMA数据流,DMA1_Stream0~7/DMA2_Stream0~7
+//ndtr:数据传输量
+void MYDMA_Enable(DMA_Stream_TypeDef *DMA_Streamx, u16 ndtr)
+{
+
+	DMA_Cmd(DMA_Streamx, DISABLE); //关闭DMA传输
+
+	while (DMA_GetCmdStatus(DMA_Streamx) != DISABLE)
+	{
+	} //确保DMA可以被设置
+
+	DMA_SetCurrDataCounter(DMA_Streamx, ndtr); //数据传输量
+
+	DMA_Cmd(DMA_Streamx, ENABLE); //开启DMA传输
+}
