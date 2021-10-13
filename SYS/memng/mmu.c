c56a827d6f8ca35095d6700cb054a167d487d6bd
8BIT0 <971168718@qq.com>
2021-10-13 12:11:42 +0800

    mmu update

--- SYS/memng/mmu.c
+++ SYS/memng/mmu.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdio.h>
 
+static Mem_Opr_State_List Mem_OprState = Mem_Init;
+
 static Mem_Monitor_TypeDef Mem_Monitor = {
     .remain_size = PHY_MEM_SIZE,
     .total_size = PHY_MEM_SIZE,
@@ -14,12 +16,48 @@ static void MMU_Trim(void)
 {
 }
 
+/* memory block initial */
+static bool MMU_InitBlock(void)
+{
+
+    return true;
+}
+
+static uint8_t MMU_ErrorProc(Mem_Opr_State_List *state)
+{
+    uint8_t state = 0;
+
+    return state;
+}
+
 /* memory manager unit malloc */
 void *MMU_Malloc(uint32_t size)
 {
-    __asm("cpsid i");
+    while (true)
+    {
+        switch ((uint8_t)Mem_OprState)
+        {
+        case Mem_Init:
+            if (MMU_InitBlock())
+            {
+                Mem_OprState = Men_Molloc;
+            }
+            else
+                Mem_OprState = Mem_Init_Error;
+            break;
 
-    __asm("cpsie i");
+        case Men_Molloc:
+            __asm("cpsid i");
+
+            __asm("cpsie i");
+            return;
+
+        default:
+            /* TO DO Error Process */
+            MMU_ErrorProc(&Mem_OprState);
+            break;
+        }
+    }
 }
 
 /* memory manager unit free */
--- SYS/memng/mmu.h
+++ SYS/memng/mmu.h
@@ -7,14 +7,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BLOCK_LENGTH
+#define BLOCK_SIZE
+
 #define PHY_MEM_SIZE 64 * 1024
+
+typedef uint32_t MemSize_t;
 
 typedef struct
 {
-    uint32_t used_size;
-    uint32_t remain_size;
-    uint32_t total_size;
+    MemSize_t used_size;
+    MemSize_t remain_size;
+    MemSize_t total_size;
 } Mem_Monitor_TypeDef;
+
+typedef enum
+{
+    Mem_Init = 0,
+    Mem_Init_Error,
+    Mem_None_Opr,
+    Men_Molloc,
+    Mem_Molloc_Successd,
+    Men_Molloc_Failed,
+    Mem_Free,
+    Mem_Free_Successd,
+    Mem_Free_Failed,
+} Mem_Opr_State_List;
 
 typedef struct
 {
