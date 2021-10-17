065b0506fd90c0c368f903710d8a23c969ec8f86
8BIT0 <971168718@qq.com>
2021-10-17 19:54:08 +0800

    mmu update

--- SYS/memng/mmu.c
+++ SYS/memng/mmu.c
@@ -23,13 +23,13 @@ static void MMU_Init(void)
     uint32_t index = 0;
 
     /* init memory state table */
-    for (index = 0; index < MEM_ALLOC_TABLE_SIZE; index++)
-    {
-        MMU_StateTable[index] = 0;
-    }
-
     for (index = 0; index < PHY_MEM_SIZE; index++)
     {
+        if (index < MEM_ALLOC_TABLE_SIZE)
+        {
+            MMU_StateTable[index] = 0;
+        }
+
         MMU_Buff[index] = 0;
     }
 }
