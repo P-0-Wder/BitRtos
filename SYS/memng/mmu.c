cd725bb05e6b977bd7fdd88abb1d6e489f893d49
8BIT0 <971168718@qq.com>
2021-10-20 13:46:04 +0800

    mmu update

--- SYS/memng/mmu.c
+++ SYS/memng/mmu.c
@@ -1,170 +1,44 @@
-/*
-* code "modify"(copy) from freertos
-* i can`t create my own memory manager module
-* sorry guys maybe one day but not now
-* sad story...
-*
-* Coder : 8_B!T0
-*/
 #include "mmu.h"
-#include <string.h>
-#include <stdio.h>
 
-#define MemBlock_MinSize sizeof(MemBlock_TypeDef) * 2
+uint8_t Mem_Buff[PHY_MEM_SIZE] __attribute__((align(BLOCK_ALIGMENT_SIZE), at(0x10000000)));
 
-static Mem_Monitor_TypeDef Mem_Monitor = {
-    .remain_size = 0,
-    .total_size = PHY_MEM_SIZE,
-    .used_size = 0,
-    .init = false,
-};
+Mem_Monitor_TypeDef Mem_Monitor;
+MemBlock_TypeDef MemStart;
+MemBlock_TypeDef *MemEnd;
 
-static uint8_t MMU_Buff[PHY_MEM_SIZE] __attribute__((aligned(BLOCK_ALIGMENT_SIZE))) __attribute__((at(0x10000000)));
-
-static MemBlock_TypeDef MMU_Start;
-static MemBlock_TypeDef *MMU_End = NULL;
-
-/* memory block initial */
-static void MMU_Init(void)
+void MMU_Init(void)
 {
-    MemBlock_TypeDef *FirstFreeBlock;
-    uint32_t index = 0;
-    MemAddr addr = 0;
-    uint8_t *aliged_addr = 0;
+    Mem_Monitor.phy_size = PHY_MEM_SIZE;
+    Mem_Monitor.used_size = 0;
 
-    /* init memory state table */
-    for (index = 0; index < PHY_MEM_SIZE; index++)
+    for (uint16_t i = 0; i < PHY_MEM_SIZE; i++)
     {
-        MMU_Buff[index] = 0;
+        Mem_Buff[i] = 0;
     }
 
-    /* aligment mmu address */
-    addr = (MemAddr)MMU_Buff;
-
-    if (addr & (BLOCK_ALIGMENT_SIZE - 1))
-    {
-        addr += BLOCK_ALIGMENT_SIZE - addr & (BLOCK_ALIGMENT_SIZE - 1);
-        addr &= ~((size_t)(BLOCK_ALIGMENT_SIZE - 1));
-
-        Mem_Monitor.total_size -= addr - (MemAddr)MMU_Buff;
-    }
-
-    aliged_addr = (uint8_t *)addr;
-
-    MMU_Start.nxt = (void *)aliged_addr;
-    MMU_Start.len = 0;
-
-    addr = ((MemAddr)aliged_addr) + Mem_Monitor.total_size;
-    addr -= sizeof(MemBlock_TypeDef);
-    addr &= ~((MemAddr)(BLOCK_ALIGMENT_SIZE - 1));
-
-    MMU_End = (void *)addr;
-    MMU_End->nxt = NULL;
-    MMU_End->len = 0;
-
-    FirstFreeBlock = (void *)aliged_addr;
-    FirstFreeBlock->len = addr - (size_t)FirstFreeBlock;
-    FirstFreeBlock->nxt = MMU_End;
-
-    Mem_Monitor.remain_size = FirstFreeBlock->len;
+    Mem_Monitor.init = true;
 }
 
-static void MMU_UpdateFreeBlock(MemBlock_TypeDef *block)
+void MMU_InsertFreeBlock(MemBlock_TypeDef *blk)
 {
-    if (MMU_Start.nxt == NULL)
-    {
-    }
 }
 
-/* memory manager unit malloc */
-void *MMU_Malloc(uint32_t size)
+void *MMU_Molloc(uint16_t size)
 {
-    MemBlock_TypeDef *Block = NULL;
-    MemBlock_TypeDef *MMU_PrvBlock = NULL;
-    MemBlock_TypeDef *MMU_NewBlockLink = NULL;
-
-    uint32_t req_block_num = 0;
-    uint32_t req_byte_size = 0;
     void *mem_addr;
-
-    __asm("cpsid i");
 
     if (!Mem_Monitor.init)
     {
         MMU_Init();
-
-        Mem_Monitor.init = true;
     }
 
-    if (Mem_Monitor.init)
+    __asm("cpsid i");
+
+    if (size <= Mem_Monitor.remain_size)
     {
-        if ((size > Mem_Monitor.remain_size) || (size == 0))
-        {
-            /* illegal parameter input */
-            mem_addr = NULL;
-        }
-        else
-        {
-            req_block_num = size / BLOCK_ALIGMENT_SIZE;
-            if (size % BLOCK_ALIGMENT_SIZE)
-            {
-                req_block_num++;
-            }
-
-            req_byte_size = req_block_num * BLOCK_ALIGMENT_SIZE;
-
-            if ((req_byte_size > 0) && (req_byte_size <= Mem_Monitor.remain_size))
-            {
-                MMU_PrvBlock = &MMU_Start;
-                Block = MMU_Start.nxt;
-                while ((Block->len < req_byte_size) && (Block->nxt != NULL))
-                {
-                    MMU_PrvBlock = Block;
-                    Block = Block->nxt;
-                }
-
-                if (Block != MMU_End)
-                {
-                    Mem_Monitor.remain_size -= req_byte_size;
-                    Mem_Monitor.used_size += req_byte_size;
-
-                    mem_addr = (void *)(((uint8_t *)MMU_PrvBlock->nxt) + sizeof(MemBlock_TypeDef));
-                    MMU_PrvBlock->nxt = Block->nxt;
-
-                    if ((Block->len - req_byte_size) > MemBlock_MinSize)
-                    {
-                        MMU_NewBlockLink = (void *)(((uint8_t *)Block) + req_byte_size);
-
-                        MMU_NewBlockLink->len = Block->len - req_byte_size;
-                        Block->len = req_byte_size;
-
-                        MMU_UpdateFreeBlock(MMU_NewBlockLink);
-                    }
-
-                    Mem_Monitor.remain_size -= Block->len;
-
-                    Block->len |= xBlockAllocatedBit;
-                    Block->nxt = NULL;
-                }
-            }
-            else
-                mem_addr = NULL;
-        }
     }
+
     __asm("cpsie i");
 
     return mem_addr;
-}
-
-/* memory manager unit free */
-void MMU_Free(void *ptr)
-{
-    __asm("cpsid i");
-
-    __asm("cpsie i");
-}
-
-Mem_Monitor_TypeDef MMU_Info(void)
-{
-    return Mem_Monitor;
 }
--- SYS/memng/mmu.h
+++ SYS/memng/mmu.h
@@ -11,11 +11,14 @@
 #define BLOCK_BORDER_SIZE 4
 #define PHY_MEM_SIZE 64 * 1024
 
-typedef uint32_t MemSize_t;
-typedef uint32_t MemAddr;
+#define MINIMUM_BLOCK_SIZE BLOCK_ALIGMENT_SIZE * 2
+
+typedef uint16_t MemSize_t;
+typedef uint32_t MemBlock_Addr;
 
 typedef struct
 {
+    MemSize_t phy_size;
     MemSize_t used_size;
     MemSize_t remain_size;
     MemSize_t total_size;
@@ -27,7 +30,7 @@ typedef struct
 typedef struct
 {
     void *nxt;
-    uint32_t len;
+    uint16_t len;
 } MemBlock_TypeDef;
 
 #endif
