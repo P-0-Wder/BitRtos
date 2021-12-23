71792f133af4f11f359d8f120b44c66a79dcd456
8BIT0 <971168718@qq.com>
2021-12-23 23:50:52 +0800

    UI_Controller update

--- App/Task_Display/Task_Widget.h
+++ App/Task_Display/Task_Widget.h
@@ -7,6 +7,25 @@
 
 typedef enum
 {
+    Boot_Page_8Bit = 0,
+    Boot_Page_BitRtos,
+    Boot_Page_ELRS,
+    Boot_Page_Sum,
+} WidgetBoot_Page_List;
+
+typedef enum
+{
+    WidGUI_Boot = 0,
+    WidGUI_Version,
+    WidGUI_OS_Info,
+    WidGUI_BaseSetting,
+    WidGUI_FileDisk,
+    WidGUI_ModelControl,
+    WidGUI_RemoteStatus,
+} TaskWidget_GUIFunc_List;
+
+typedef enum
+{
     TaskWdiget_State_Init,
     TaskWidget_State_CheckFresh,
     TaskWidget_State_Fresh,
