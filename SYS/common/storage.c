399e96be7c589713854bb4c47df8e306d8858ae2
8BIT0 <971168718@qq.com>
2021-12-26 18:14:44 +0800

    storage update

--- SYS/common/storage.c
+++ SYS/common/storage.c
@@ -1,38 +1,49 @@
 #include "storage.h"
+#include "periph_flash.h"
 #include "crc16.h"
 
-typedef int8_t Stor_error;
+typedef Storage_ErrorCode_List Stor_error;
 
 static bool Storage_Init(void)
 {
 }
 
-static Stor_error Stroage_Get(uint8_t sector, uint8_t ID, uint8_t *data, uint16_t len)
+static bool Storage_BaseWrite(uint32_t addr, uint8_t *data, uint16_t len)
 {
     return true;
+}
+
+static bool Storage_BaseRead(uint32_t addr, uint8_t *data, uint16_t len)
+{
+    return true;
+}
+
+static Stor_error Stroage_Get(uint8_t sector, uint8_t ID, uint8_t *data, uint16_t len)
+{
+    return Storage_NoError;
 }
 
 static Stor_error Storage_Set(uint8_t sector, uint8_t ID, uint8_t *data, uint16_t len)
 {
-    return true;
+    return Storage_NoError;
 }
 
-static Stor_error Storage_GetUserParam()
+static Stor_error Storage_GetUserParam(uint8_t ID, uint8_t *data, int16_t len)
 {
-    return true;
+    return Storage_NoError;
 }
 
-static Stor_error Storage_SetUserParam()
+static Stor_error Storage_SetUserParam(uint8_t ID, uint8_t *data, uint16_t len)
 {
-    return true;
+    return Storage_NoError;
 }
 
-static Stor_error Storage_GetSystemParam()
+static Stor_error Storage_GetSystemParam(uint8_t ID, uint8_t *data, uint16_t len)
 {
-    return true;
+    return Storage_NoError;
 }
 
-static Stor_error Storage_SetSysetmParam()
+static Stor_error Storage_SetSysetmParam(uint8_t ID, uint8_t *data, uint16_t len)
 {
-    return true;
+    return Storage_NoError;
 }
